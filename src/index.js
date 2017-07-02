a3b1da24eb68e42463b451e837ef1de39b54ccfd
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-02 05:55:52 -0700

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import Routes from './routes'
-import registerServiceWorker from './registerServiceWorker';
+import configureStore from './redux/configure-store'
+// import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('root')
+);
+// registerServiceWorker();
---
+++ src/redux/initial-state.js
@@ -0,0 +1,5 @@
+const initialState = {
+  name: 'Kyle',
+}
+
+export default initialState
