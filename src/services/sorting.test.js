f277c5082fc1a504025bee67e88d4ae92723dee2
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-06 14:13:48 -0700

    Fix sorting service test

--- src/services/sorting.test.js
+++ src/services/sorting.test.js
@@ -72,6 +72,7 @@ describe('sorting', () => {
           costPerOunceOfPureAlcohol: 1.2757201646090535
         },
       ]
+      expect(sortedByTotalCostHighToLow).toEqual(expectedSortedByTotalCostHighToLow)
     })
   })
 })
